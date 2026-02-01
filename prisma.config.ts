33c7207416294cb031278c2776ec992c99ee0a0d
pianist22 <priyanshusaha944@gmail.com>
2026-02-01 22:51:30 +0530

    Typescript Issue handled

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,12 +3,18 @@
 import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
+const DATABASE_URL = process.env.DATABASE_URL;
+
+if(!DATABASE_URL) {
+  throw new Error("Missing DATABASE_URL environment variable");
+}
+
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
   },
   datasource: {
-    url: process.env["DATABASE_URL"],
+    url: DATABASE_URL,
   },
 });
